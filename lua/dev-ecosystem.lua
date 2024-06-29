5889e89c50ebad7c332d61d0bb6f9d5b1f4d0c40
= <=>
2024-06-29 23:58:59 +0200

    feat(Setup): Set clients on_attach

--- lua/dev-ecosystem.lua
+++ lua/dev-ecosystem.lua
@@ -2,7 +2,8 @@ local parse_config = require 'dev-ecosystem.config.parse'
 local register_clients = require 'dev-ecosystem.client.register'
 
 --- @class Context
---- @field capabilities? lsp.ServerCapabilities
+--- @field capabilities? lsp.ClientCapabilities
+--- @field on_attach? fun(client: vim.lsp.Client)
 
 local M = {}
 
--- lua/dev-ecosystem/client/start.lua
+++ lua/dev-ecosystem/client/start.lua
@@ -1,10 +1,10 @@
---- Start or attach to a client for a given config
+--- Get LSP minimal VIM client config from defined server config
 --- @param config ServerConfig
---- @param context Context
-local function start(config, context)
+--- @return vim.lsp.ClientConfig
+local function minimal_client_config(config)
   local cwd = vim.fn.getcwd()
 
-  local lsp_config = {
+  return {
     name = config.name,
     cmd = {
       "docker", "run", "--rm", "--interactive",
@@ -14,12 +14,24 @@ local function start(config, context)
       config.cmd
     }
   }
+end
+
+--- Start or attach to a client for a given config
+--- @param server_config ServerConfig
+--- @param context Context
+local function start_server(server_config, context)
+
+  local lsp_config = minimal_client_config(server_config)
 
   if context.capabilities then
     lsp_config.capabilities = context.capabilities
   end
 
+  if context.on_attach then
+    lsp_config.on_attach = context.on_attach
+  end
+
   vim.lsp.start(lsp_config)
 end
 
-return start
+return start_server
